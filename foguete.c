46a6a9e8d4bc29e0550be562e69e269d714cac5b
Artur Belo <arturbelo2102@gmail.com>
2026-03-28 15:51:33 -0300

    identando arquivo

--- foguete.c
+++ foguete.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 int main(){
-int contador = 5
+    int contador = 5;
 
-while( contador = 0 ) {
-printf("%d ", contador);
-contador = contador - 1;
-}
+    while( contador > 0 ) {
+        printf("%d\n", contador);
+        contador = contador - 1;
+        if(contador == 0) {
+            printf("Foguete decolou!");
+            break;
+        }
+    }
 
-
+    return 0;
 }
